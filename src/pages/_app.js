65c0660c470b8a5571a22a4c01342cb653cc0b5b
justalk <justal.kevin@gmail.com>
2021-12-19 18:45:51 +0100

    [FEATURE] Adding the link and design

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,21 @@
-import styles from '@src/styles/globals.scss'
+import Link from 'next/link'
+import '@src/styles/globals.scss'
+import styles from './styles.module.scss'
 
-function MyApp ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function Home ({ Component, pageProps }) {
+  return (
+    <div className={styles.home}>
+      <div className={styles.home_left}>
+        <ul>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/experience_00000">Experience 00000</Link></li>
+        </ul>
+      </div>
+      <div className={styles.home_right}>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  )
 }
 
-export default MyApp
+export default Home
